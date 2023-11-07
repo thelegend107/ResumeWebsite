93c02a995e2882328703137796d7440ab3b6c14e
Moe Ayoub <mma.ayoub@outlook.com>
2023-11-07 04:59:55 -0600

    Merge pull request #6 from thelegend107/Initial-Development
    A little tweak 2

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -13,7 +13,9 @@ defineProps({
     <h2 class="electricPurple">
       Software Developer
     </h2>
-      Electrical Engineer graduate of South Dakota State University (SDSU) with 4 years of diversified experience and strong software development skills including in-depth knowledge of Software Development Life Cycle and practical experience with SCRUM and Agile Software Development Model.
+      <!-- Electrical Engineer graduate of South Dakota State University (SDSU) with 4 years of diversified experience and strong software development skills including in-depth knowledge of Software Development Life Cycle and practical experience with SCRUM and Agile Software Development Model. -->
+      As a Software Developer at Raven Industries, I was the product owner and main contributor to our E-Commerce web site for the Agricultural Division. I also integrated our web site, CRM, ERP, and IT ticketing systems using various technologies, such as C#, Azure, Boomi, and Oracle. 
+      <br><br> I graduated from South Dakota State University with a BSEE degree in 2020, where I designed and built a device that can reconfigure the network settings of display controllers for Daktronics. I have a strong background in electrical and electronics engineering, as well as object-oriented programming and web development. I am passionate about creating innovative and user-friendly solutions that enhance the performance and efficiency of complex systems.
   </div>
 </template>
 
