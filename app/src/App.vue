e284288ae7835ef5f351878b56f0c12d5e25407d
Moe Ayoub <mma.ayoub@outlook.com>
2023-11-27 13:28:15 -0600

    Development-4

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,90 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import LinkedInIcon from './components/icons/IconLinkedIn.vue'
-import GitHubIcon from './components/icons/IconGitHub.vue'
-import EmailIcon from './components/icons/IconEmail.vue'
-import DownloadIcon from './components/icons/IconDownload.vue'
+import { inject } from 'vue'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiAccount, mdiLinkedin, mdiGithub, mdiFilePdfBox, mdiEmailOutline } from '@mdi/js'
+
+const resumeAPI = inject('resume');
 </script>
 
 <template>
   <header>
     <div>
-      <a href="https://gravatar.com/mmaayoub" target="_blank" rel="noopener noreferrer"><img alt="Moe Profile" class="logo" src=".\assets\profilePicture-modified.png" width="250" height="250" /></a>
+      <img alt="Moe Profile" class="logo" src=".\assets\profilePicture-modified.png" width="250" height="250" />
       <table class="social-stack">
-        <td class="social"><a href="https://www.linkedin.com/in/mhayoub/" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a></td>
-        <td class="social"><a href="https://github.com/thelegend107" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a></td>
-        <td class="social"><a href="mailto:mma.ayoub@outlook.com" target="_blank" rel="noopener noreferrer"><EmailIcon /></a></td>
-        <td class="social"><a href="/M.Ayoub(Moe)_Resume.pdf" target="_blank" rel="noopener noreferrer" download><DownloadIcon /></a></td>
+        <td class="social"><a href="https://gravatar.com/mmaayoub" target="_blank" rel="noopener noreferrer"><svg-icon type="mdi" :path="mdiAccount" :size="25" /></a></td>
+        <td class="social"><a href="https://www.linkedin.com/in/mhayoub/" target="_blank" rel="noopener noreferrer"><svg-icon type="mdi" :path="mdiLinkedin" :size="25" /></a></td>
+        <td class="social"><a href="https://github.com/thelegend107" target="_blank" rel="noopener noreferrer"><svg-icon type="mdi" :path="mdiGithub" :size="25" /></a></td>
+        <td class="social"><a href="mailto:mma.ayoub@outlook.com" target="_blank" rel="noopener noreferrer"><svg-icon type="mdi" :path="mdiEmailOutline" :size="25" /></a></td>
+        <td class="social"><a href="/M.Ayoub(Moe)_Resume.pdf" target="_blank" rel="noopener noreferrer" download><svg-icon type="mdi" :path="mdiFilePdfBox" :size="25" /></a></td>
       </table>
-    </div>
-    <div class="wrapper">
-      <HelloWorld msg="Moe Ayoub" />
     </div>
   </header>
   <main>
-    <TheWelcome />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-  border: 3px solid hsla(273, 98%, 60%, 1);
-  border-radius: 50%;
-}
-
-.social {
-  padding-inline: 1rem;
-} 
-
-.social-stack {
-  position: inherit;
-  margin: 0 auto 1rem;
-}
-
-.social-stack td a:hover, .social-stack td a:focus {
-  background-color: transparent;
-}
-
 
 @media (min-width: 1024px) {
-  header {
-    width: 100%;
-    height: 100vh;
-    min-height: 100vh;
-    top: 0; /* this is required for "sticky" to work */
-    position: sticky;
-    padding-top: calc(var(--section-gap) / 2);
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  main {
-    padding-top: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 1rem 0 0 8rem;
-  }
-
-  .social-stack {
-    position: inherit;
-    margin: 1rem 0 2rem 8rem;
-  }
-
-  .social {
-    padding-left: 20px;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+const resume = await axios.get("api/user/mma.ayoub@outlook.com");
+
+const app = createApp(App);
+
+app.provide('resume', resume);
+
+app.mount('#app');
