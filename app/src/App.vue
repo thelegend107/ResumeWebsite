c4e0844eb0374e4002394b89cb9c9e4f68560bf6
Moe Ayoub <mma.ayoub@outlook.com>
2023-12-12 16:48:46 -0600

    Merge pull request #17 from thelegend107/Development-5

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,19 +2,12 @@
 import { ref } from 'vue'
 import Header from './components/Header.vue';
 import SideBar from './components/SideBar.vue';
-import { getUserInfo } from './utils';
 
-const userLogin = ref(null);
 const sideBarWidth = ref("0px");
 
 function showSidebar() { sideBarWidth.value = "300px"};
 function collapseSidebar() { sideBarWidth.value = "0px"};
 
-getUserInfo().then(response => {
-    userLogin.value = response
-}).catch(error => {
-    console.log(error);
-});
 </script>
 
 <template>
